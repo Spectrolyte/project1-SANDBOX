946b8d50907ef310906ba887fa0da99845903ede
Diana <dianaltecson@gmail.com>
2017-11-14 10:56:56 -0800

    please work

--- assets/javascript/game-logic.js
+++ assets/javascript/game-logic.js
@@ -177,7 +177,11 @@ $('#submit-btn').click(function (event) {
 // add firebase obj listener for user data -- array changes
 // whenever a user's array is updated, update changes here
 
-database.ref('users/' + UID).on('value', function (snapshot) {
+
+database.ref('/users/' + UID).on('value', function (snapshot) {
+	// var key = snapshot.key; // "UID" -- whatever it may be
+    // var childKey = snapshot.child("guesses").key; // 
+
 	console.log('HELLO I WORKED');
 	console.log(snapshot);
 	console.log(snapshot.val());
